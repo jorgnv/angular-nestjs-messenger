a7408d71b949be023d1403befa14d108dd583122
Jorge Nava <jorge@seeed.us>
2023-08-14 21:09:12 -0500

    configuracion typeorm

--- backend-nestjs/src/app.module.ts
+++ backend-nestjs/src/app.module.ts
@@ -1,10 +1,32 @@
-import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import configuration from './config/configuration';
 import { MessagesModule } from './messages/messages.module';
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [MessagesModule],
+  imports: [
+    MessagesModule,
+    ConfigModule.forRoot({
+      load: [configuration],
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService], // Inject ConfigService
+      useFactory: async (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_DATABASE'),
+        entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+        synchronize: true,
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- backend-nestjs/src/main.ts
+++ backend-nestjs/src/main.ts
@@ -1,9 +1,17 @@
-import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { NestFactory } from '@nestjs/core';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.enableCors();
-  await app.listen(3000);
+
+  const configService = app.get(ConfigService);
+
+  const PORT = configService.get<number>('port');
+  await app.listen(PORT, async () => {
+    console.log(`🚀 Server on port ${PORT}`);
+  });
 }
 bootstrap();
